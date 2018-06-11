994e5088d77229bba2a0ae3fa0df271f8987869f
Jamie Perkins <jamieperkins@Computa.local>
2018-06-11 17:08:55 -0600

    simplify for speed

--- src/app/hometown/hometown.component.ts
+++ src/app/hometown/hometown.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { MapMarker } from '../map/map.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-hometown',
   templateUrl: './hometown.component.html',
   styleUrls: ['./hometown.component.scss']
 })
-export class HometownComponent implements OnInit {
+export class HometownComponent {
 
-  constructor() { }
+  userMarker$: Observable<MapMarker>;
 
-  ngOnInit() {
+  constructor(private userService: UserService, private afs: AngularFirestore) {
+    this.userMarker$ = this.afs.doc<MapMarker>(`hometowns/${this.userService.currentUser.uid}`).valueChanges();
   }
 
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,8 @@
-import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
-import { firestore } from 'firebase';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MouseEvent } from '@agm/core';
 import { UserService } from '../user.service';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { MapService, MapMarker } from '../map.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,15 +10,13 @@ import { Observable } from 'rxjs';
   styleUrls: ['./map.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class MapComponent implements OnInit, OnDestroy {
+export class MapComponent {
 
-  geoPoint: firestore.GeoPoint;
+  lat = 37;
+  lng = -97;
   markers$: Observable<MapMarker[]>;
-  zoom = 5;
-  currentUser;
 
   constructor(
-    public mapService: MapService,
     public userService: UserService,
     public afAuth: AngularFireAuth,
     private afs: AngularFirestore
@@ -28,26 +24,21 @@ export class MapComponent implements OnInit, OnDestroy {
     this.markers$ = afs.collection<MapMarker>('hometowns').valueChanges();
   }
 
-  ngOnInit() {
-    this.mapService.geoPoint$.subscribe(geo => this.geoPoint = geo);
-  }
-
-  ngOnDestroy() {
-    this.mapService.geoPoint$.unsubscribe();
-  }
-
   setHometown(event: MouseEvent) {
     console.log('map click', event.coords);
     if (this.userService.isLoggedIn()) {
       const marker: MapMarker = {
-        geoPoint: new firestore.GeoPoint(event.coords.lat, event.coords.lng),
+        lat: event.coords.lat,
+        lng: event.coords.lng,
         label: this.userService.currentUser.displayName
       };
       this.afs.doc(`hometowns/${this.userService.currentUser.uid}`).set(marker);
     }
   }
+}
 
-  updatePosition(m: MapMarker, event: MouseEvent) {
-    this.setHometown(event);
-  }
+export interface MapMarker {
+  lat: number;
+  lng: number;
+  label: string;
 }
